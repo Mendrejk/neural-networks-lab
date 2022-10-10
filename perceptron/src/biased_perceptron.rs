930c78dc9f5d3325740089f32608ed19d843116e
sebastian.lakomy <sebastian.lakomy@infor.com>
2022-10-10 11:58:18 +0200

    Add second task

--- perceptron/src/biased_perceptron.rs
+++ perceptron/src/biased_perceptron.rs
@@ -3,14 +3,15 @@ use crate::DataEntry;
 pub struct BiasedPerceptron {
     pub x_weight: f32,
     pub y_weight: f32,
-    pub bias_weight: Option<f32>,
+    pub bias_weight: f32,
 }
 
 impl BiasedPerceptron {
     pub fn learn(&mut self, data_entry: &DataEntry, learn_factor: f32) -> bool {
-        let stimulus = data_entry.x * self.x_weight + data_entry.y * self.y_weight + self.bias_weight;
+        let stimulus =
+            data_entry.x * self.x_weight + data_entry.y * self.y_weight + self.bias_weight;
 
-        let result = if stimulus > 0 { 1 } else { 0 };
+        let result = if stimulus > 0.0 { 1 } else { 0 };
         let delta = (data_entry.expected_result - result) as f32;
 
         self.x_weight += learn_factor * delta * data_entry.x;
--- perceptron/src/main.rs
+++ perceptron/src/main.rs
@@ -2,37 +2,41 @@ use crate::data_entry::DataEntry;
 use perceptron::Perceptron;
 
 use std::fs;
+use rand::Rng;
+use crate::biased_perceptron::BiasedPerceptron;
 
+mod biased_perceptron;
 mod data_entry;
 mod perceptron;
-mod biased_perceptron;
 
 fn main() {
     let data_entries = read_data();
 
-    first_task(&data_entries);
+    // first_task(&data_entries);
+    second_task(&data_entries);
 }
 
 fn first_task(data_entries: &Vec<DataEntry>) {
-    let learn_factor = 0.1;
-    let mut perceptron = Perceptron {
-        x_weight: 0.01,
-        y_weight: 0.01,
-    };
+    let learn_factor = 0.01;
 
-    for theta in [0.2, 0.4, 0.6, 0.8, 0.9, 1.0, 1.2] {
+    for theta in [0.05, 0.2, 0.4, 0.6, 0.8, 0.9, 1.0, 1.2] {
         let mut x_weights: Vec<f32> = vec![];
         let mut y_weights: Vec<f32> = vec![];
         let mut epochs: Vec<u32> = vec![];
 
-        let mut current_epochs = 0;
-        let mut got_wrong_answer = true;
-
         for _experiment_iter in 0..10 {
+            let mut got_wrong_answer = true;
+            let mut current_epochs = 0;
+
+            let mut perceptron = Perceptron {
+                x_weight: 0.01,
+                y_weight: 0.01,
+            };
+
             while got_wrong_answer {
                 got_wrong_answer = false;
 
-                for entry in &data_entries {
+                for entry in data_entries {
                     if !perceptron.learn(entry, learn_factor, theta) {
                         got_wrong_answer = true;
                     }
@@ -40,16 +44,77 @@ fn first_task(data_entries: &Vec<DataEntry>) {
 
                 current_epochs += 1;
             }
+
+            epochs.push(current_epochs);
+            x_weights.push(perceptron.x_weight);
+            y_weights.push(perceptron.y_weight);
         }
 
-        println!("epochs: {}", epochs); // TODO remove those 3
-        println!("x weights: {}", x_weights);
-        println!("y weights: {}", y_weights);
+        let avg_epochs = epochs.iter().sum::<u32>() as f32 / epochs.len() as f32;
+        let avg_x_weight = x_weights.iter().sum::<f32>() / x_weights.len() as f32;
+        let avg_y_weight = y_weights.iter().sum::<f32>() / y_weights.len() as f32;
+        println!(
+            "theta: {}, avg epochs: {}, x weight: {}, y weight: {}",
+            theta, avg_epochs, avg_x_weight, avg_y_weight
+        );
+    }
+}
+
+fn second_task(data_entries: &Vec<DataEntry>) {
+    let learn_factor = 0.01;
+    let mut rng = rand::thread_rng();
+
+    for weight_range in [
+        -1.0..1.0,
+        -0.8..0.8,
+        -0.5..0.5,
+        -0.2..0.2,
+        -0.1..0.1,
+        -0.05..0.05,
+        -0.01..0.01,
+    ] {
+        let mut x_weights: Vec<f32> = vec![];
+        let mut y_weights: Vec<f32> = vec![];
+        let mut bias_weights: Vec<f32> = vec![];
+        let mut epochs: Vec<u32> = vec![];
+
+        for _experiment_iter in 0..10 {
+            let mut got_wrong_answer = true;
+            let mut current_epochs = 0;
+
+            let mut perceptron = BiasedPerceptron {
+                x_weight: rng.gen_range(weight_range.clone()),
+                y_weight: rng.gen_range(weight_range.clone()),
+                bias_weight: rng.gen_range(weight_range.clone())
+            };
+
+            while got_wrong_answer {
+                got_wrong_answer = false;
+
+                for entry in data_entries {
+                    if !perceptron.learn(entry, learn_factor) {
+                        got_wrong_answer = true;
+                    }
+                }
+
+                current_epochs += 1;
+            }
+
+            epochs.push(current_epochs);
+            x_weights.push(perceptron.x_weight);
+            y_weights.push(perceptron.y_weight);
+            bias_weights.push(perceptron.bias_weight);
+        }
 
         let avg_epochs = epochs.iter().sum::<u32>() as f32 / epochs.len() as f32;
-        let avg_x_weight = x_weights.iter().sum::<f32>() / x_weight.len() as f32;
-        let avg_y_weight = y_weights.iter().sum::<f32>() / y_weight.len() as f32;
-        println!("theta: {}, avg epochs: {}, x weight: {}, y weight: {}", theta, avg_epochs, avg_x_weight, avg_y_weight);
+        let avg_x_weight = x_weights.iter().sum::<f32>() / x_weights.len() as f32;
+        let avg_y_weight = y_weights.iter().sum::<f32>() / y_weights.len() as f32;
+        let avg_bias_weight = bias_weights.iter().sum::<f32>() / bias_weights.len() as f32;
+
+        println!(
+            "weight range: {:?}, avg epochs: {}, x weight: {}, y weight: {}, bias weight: {}",
+            weight_range, avg_epochs, avg_x_weight, avg_y_weight, avg_bias_weight
+        );
     }
 }
 
